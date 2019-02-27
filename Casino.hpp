4946b6dc6c9a69c6373e37a6e3afd43705ec6204
Andrew Mitchell <andymitch559@gmail.com>
2019-02-27 12:49:28 -0700

    Create Casino.hpp

---
+++ Casino.hpp
@@ -0,0 +1,16 @@
+/*
+struct table
+struct player (universal)
+class player:poker
+class player:blackjack
+*/
+
+//consolidate Poker.hpp and Blackjack.hpp to this Casino.hpp
+
+/*
+Casino.cpp
+    ^
+Casino.hpp > Poker.cpp
+           > PokerRules.cpp
+           > Blackjack.cpp
+*/
